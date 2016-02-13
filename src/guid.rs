cc1084b25c96486fb075447e3316df1a32bd54fa
Nick Owens <mischief@offblast.org>
2016-02-13 14:28:13 -0800

    mark Guid fields pub

--- src/guid.rs
+++ src/guid.rs
@@ -1,6 +1,6 @@
 /// Type for EFI_GUID.
 #[repr(C)]
-pub struct Guid(u32, u16, u16, [u8; 8]);
+pub struct Guid(pub u32, pub u16, pub u16, pub [u8; 8]);
 
 /// GUID for UEFI protocol for loaded images
 pub static EFI_LOADED_IMAGE_PROTOCOL_GUID: Guid = Guid(0x5B1B31A1, 0x9562, 0x11d2, [0x8E,0x3F,0x00,0xA0,0xC9,0x69,0x72,0x3B]);
