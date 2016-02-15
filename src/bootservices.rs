9a381904c85a18d8274a90de96591d717f86925e
Nick Owens <mischief@offblast.org>
2016-02-15 11:25:37 -0800

    bootservices: unwrap BootServices, misc things, use Protocol trait for Handle funcs

--- src/bootservices.rs
+++ src/bootservices.rs
@@ -1,31 +1,41 @@
+use core::ptr;
+use core::mem;
+
 use void::{NotYetDef, CVoid};
-use base;
+use base::{Event, Handle, Handles, MemoryType, Status, Time};
 use guid;
 use table;
 use systemtable;
 
+#[repr(C)]
+pub enum LocateSearchType {
+    AllHandles = 0,
+    ByRegisterNotify = 1,
+    ByProtocol = 2,
+}
+
 /// See http://wiki.phoenix.com/wiki/index.php/EFI_BOOT_SERVICES
 #[repr(C)]
-pub struct BootServicesInternal {
+pub struct BootServices {
     header: table::TableHeader,
     raise_tpl: *const NotYetDef,
     restore_tpl: *const NotYetDef,
     allocate_pages: *const NotYetDef,
     free_pages: *const NotYetDef,
     get_memory_map: *const NotYetDef,
-    allocate_pool: *const NotYetDef,
-    free_pool: *const NotYetDef,
+    allocate_pool: unsafe extern "win64" fn(pool_type: MemoryType, size: usize, out: *mut *mut u8) -> Status,
+    free_pool: unsafe extern "win64" fn(*mut CVoid),
     create_event: *const NotYetDef,
     set_timer: *const NotYetDef,
     // typedef EFI_STATUS (EFIAPI *EFI_WAIT_FOR_EVENT) (IN UINTN NumberOfEvents, IN EFI_EVENT *Event, OUT UINTN *Index);
-    wait_for_event: unsafe extern "win64" fn(usize, *const base::Event, *mut usize) -> base::Status,
+    wait_for_event: unsafe extern "win64" fn(usize, *const Event, *mut usize) -> Status,
     signal_event: *const NotYetDef,
     close_event: *const NotYetDef,
     check_event: *const NotYetDef,
     install_protocol_interface: *const NotYetDef,
     reinstall_protocol_interface: *const NotYetDef,
     uninstall_protocol_interface: *const NotYetDef,
-    handle_protocol: unsafe extern "win64" fn(base::Handle, &guid::Guid, &mut *mut CVoid) -> base::Status,
+    handle_protocol: unsafe extern "win64" fn(Handle, &guid::Guid, &mut *mut CVoid) -> Status,
     __reserved: *const NotYetDef,
     register_protocol_notify: *const NotYetDef,
     locate_handle: *const NotYetDef,
@@ -37,15 +47,15 @@ pub struct BootServicesInternal {
     unload_image: *const NotYetDef,
     exit_boot_services: *const NotYetDef,
     get_next_monotonic_count: *const NotYetDef,
-    stall: unsafe extern "win64" fn(usize) -> base::Status,
+    stall: unsafe extern "win64" fn(usize) -> Status,
     set_watchdog_timer: *const NotYetDef,
     connect_controller: *const NotYetDef,
     disconnect_controller: *const NotYetDef,
     open_protocol: *const NotYetDef,
-    close_protocol: *const NotYetDef,
+    close_protocol: unsafe extern "win64" fn(handle: Handle, protocol: &guid::Guid, agent_handle: Handle, controller_handle: Handle) -> Status,
     open_protocol_information: *const NotYetDef,
     protocols_per_handle: *const NotYetDef,
-    locate_handle_buffer: *const NotYetDef,
+    locate_handle_buffer: unsafe extern "win64" fn(search_type: LocateSearchType, protocol: &guid::Guid, search_key: *const CVoid, nhandles: *mut usize, handles: *mut *mut CVoid) -> Status,
     locate_protocol: *const NotYetDef,
     install_multiple_protocol_interfaces: *const NotYetDef,
     uninstall_multiple_protocol_interfaces: *const NotYetDef,
@@ -55,63 +65,90 @@ pub struct BootServicesInternal {
     create_event_ex: *const NotYetDef,
 }
 
-#[repr(C)]
-pub struct BootServices(&'static BootServicesInternal);
-
 impl BootServices {
-    pub fn wait_for_event(&self, events: &[base::Event]) -> Result<usize, base::Status> {
+    pub fn free_pool<T>(&self, p: *const T) {
+        unsafe {
+            (self.free_pool)(p as *mut CVoid);
+        }
+    }
+
+    pub fn wait_for_event(&self, events: &[Event]) -> Result<usize, Status> {
         // XXX: asserts sizeof *Cvoid == sizeof Event
         if false {
             use core::mem;
-            unsafe { mem::transmute::<*const CVoid, base::Event>(0x1 as *const CVoid); }
+            unsafe { mem::transmute::<*const CVoid, Event>(0x1 as *const CVoid); }
         }
 
         let mut index : usize = 0;
 
-        let result = unsafe { (self.0.wait_for_event)(events.len(), events.as_ptr(), &mut index) };
-        if result != base::Status::Success {
+        let result = unsafe { (self.wait_for_event)(events.len(), events.as_ptr(), &mut index) };
+        if result != Status::Success {
             return Err(result);
         }
 
         Ok(index)
     }
 
-    // fix me
-    pub fn handle_protocol(&self, handle: base::Handle, uuid: &guid::Guid) -> *const super::LoadedImageProtocol {
+    pub fn handle_protocol<T: ::Protocol>(&self, handle: Handle) -> Result<&'static T, Status> {
         let mut ptr : *mut CVoid = 0 as *mut CVoid;
+        let guid = T::guid();
 
-        let bs = self.0;
 
         unsafe {
-            let status = (bs.handle_protocol)(handle, uuid, &mut ptr);
-            if status != base::Status::Success {
-                use console::SimpleTextOutput;
-                let st = systemtable::get_system_table();
-                st.console().write("bs> bad res from handle_protocol!\n");
+            let status = (self.handle_protocol)(handle, guid, &mut ptr);
+            if status != Status::Success {
+                return Err(status);
             }
         }
 
-        return ptr as *const super::LoadedImageProtocol;
+        let r = unsafe { mem::transmute::<*mut CVoid, &'static T>(ptr) };
+        Ok(r)
+    }
+
+    // TODO: for the love of types, fix me
+    pub fn close_protocol<T: ::Protocol>(&self, handle: Handle, agent_handle: Handle, controller_handle: Handle) -> Status {
+        let guid = T::guid();
+
+        unsafe {
+            (self.close_protocol)(handle, guid, agent_handle, controller_handle)
+        }
+    }
+
+    /// Retrives a slice of handles by protocol GUID.
+    pub fn locate_handle_by_protocol<T: ::Protocol>(&self) -> Result<Handles, Status> {
+        let mut nhandles : usize = 0;
+        let mut handles : *mut CVoid = ptr::null_mut();
+        let guid = T::guid();
+
+        let res = unsafe { (self.locate_handle_buffer)(LocateSearchType::ByProtocol, guid, ptr::null(), &mut nhandles as *mut usize, &mut handles) };
+
+        if res != Status::Success {
+            return Err(res);
+        }
+
+        return Ok(Handles::new(handles as *mut Handle, nhandles));
     }
 
     /// Sleep for a number of microseconds.
     pub fn stall(&self, microseconds: usize) {
         unsafe {
-            (self.0.stall)(microseconds);
+            (self.stall)(microseconds);
         }
     }
 
+    /// Copy memory, similar to memcpy.
     pub fn copy_mem(&self, dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
         unsafe {
-            (self.0.copy_mem)(dest as *mut CVoid, src as *mut CVoid, n);
+            (self.copy_mem)(dest as *mut CVoid, src as *mut CVoid, n);
         }
 
         return dest;
     }
 
+    /// Set memory, similar to memset.
     pub fn set_mem(&self, s: *mut u8, c: u8, n: usize) -> *mut u8 {
         unsafe {
-            (self.0.set_mem)(s as *mut CVoid, n, c);
+            (self.set_mem)(s as *mut CVoid, n, c);
         }
 
         return s;
