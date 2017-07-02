b26a12109787a574176dd65aba7209eb836cb32b
KANATSU Minoru <dev@orum.in>
2017-07-02 15:57:21 +0900

    lib: add library method for get_memory_map

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ mod runtimeservices;
 mod protocols;
 mod console;
 
-pub use base::{Handle, Handles, Event, MemoryType, Status, Time};
+pub use base::{Handle, Handles, Event, MemoryType, MemoryDescriptor, Status, Time};
 pub use guid::*;
 
 pub use systemtable::*;
@@ -23,6 +23,8 @@ pub use protocols::*;
 pub use runtimeservices::{ResetType, RuntimeServices};
 
 pub use console::{Attribute, ForegroundColor, BackgroundColor, InputKey, SimpleTextOutput, SimpleTextInput, Console};
+
+use core::mem;
 
 static mut POOL_ALLOCATION_TYPE: base::MemoryType = base::MemoryType::BootServicesData;
 
@@ -40,4 +42,17 @@ pub fn initialize_lib(hdl: &base::Handle, sys: &systemtable::SystemTable) {
 
 pub fn get_pool_allocation_type() -> base::MemoryType {
     unsafe{ POOL_ALLOCATION_TYPE.clone() }
+}
+
+// return (memory_map, memory_map_size, map_key, descriptor_size, descriptor_version)
+pub fn lib_memory_map() -> (&'static MemoryDescriptor,  usize, usize, usize, u32) {
+    let bs = systemtable::get_system_table().boot_services();
+    let mut buffer_size: usize = mem::size_of::<MemoryDescriptor>();
+
+    loop {
+        match unsafe { bs.get_memory_map(&mut buffer_size) } {
+            Ok(val) => return val,
+            Err(_) => { continue; },
+        };
+    }
 }
